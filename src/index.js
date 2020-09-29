b09a287cb6078056f88699a4f201e5d1fe97e16f
skamok <sk_amok@mail.ru>
2020-09-29 18:50:04 +0300

    init: add solve for tsk typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array)) {
+  if (array.length === 0) {return 0};  
+  let minNumber = array[0];
+  array.forEach(element => {
+    if (element < minNumber) { minNumber = element};
+  });
+  return minNumber;
+} else
+ {return 0};
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array)) {
+    if (array.length === 0) {return 0};
+  let maxNumber = array[0];
+  array.forEach(element => {
+    if (element > maxNumber) { maxNumber = element};
+  });
+  return maxNumber;
+} else
+{return 0};
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array)) {
+    if (array.length === 0) {return 0};
+  let summ = 0;
+  array.forEach(element => {
+    summ += element;
+  });
+  return summ/(array.length);
+} else
+{return 0};
 }
